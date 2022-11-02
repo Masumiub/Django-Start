66ba57e90d7cdb2e92028c8f215e92ceed59ee42
Md. Masum Musfique <66321598+Masumiub@users.noreply.github.com>
2022-11-03 01:30:39 +0600

    update 2

--- home/views.py
+++ home/views.py
@@ -1,17 +1,34 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'variable': "This is sent"
+    }
+    return render(request, 'index.html', context)
     #return HttpResponse("This is our Homepage")
 
 def about(request):
-    return HttpResponse("This is our About page")
+    return render(request, 'about.html')
+    #return HttpResponse("This is our About page")
 
 def services(request):
-    return HttpResponse("This is our Services page")
+    return render(request, 'services.html')
+    #return HttpResponse("This is our Services page")
 
 def contact(request):
-    return HttpResponse("This is our Contact page")
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name = name, email = email, phone=phone, desc= desc, date = datetime.today())
+        contact.save()
+        messages.success(request, 'Your Message has been sent.')
+    return render(request, 'contact.html')
+    #return HttpResponse("This is our Contact page")
